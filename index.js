30a710991dd81d42c0b3d99f1ff76ebc1b576732
taesoo0 <nm30207@gmail.com>
2023-06-27 16:52:24 +0900

    server open

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Say world");
+import exporess from "express";
+
+const app = exporess();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("서버가 열려있습니다.");
+});
+
+const userLogin = (req, res) => {
+  return res.send("유저 로그인 페이지입니다.");
+};
+
+app.get("/login", userLogin);
+
+app.listen(port);
